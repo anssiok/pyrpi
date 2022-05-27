ab155cca9141779f7e4e6f45905007442d4c82f6
Anssi Kanninen <anssi@iki.fi>
2022-05-27 10:47:45 +0300

    bugfix

--- ruuvi_fan.py
+++ ruuvi_fan.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import RPi.GPIO as GPIO
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
-import configparser
+import configparser, sys
 from datetime import datetime
 from influxdb import InfluxDBClient
 
@@ -15,17 +15,16 @@ listen = config.get('General', 'listen')
 temp_max = float(config.get('General', 'temp_max'))
 temp_min = float(config.get('General', 'temp_min'))
 
+pin = int(config.get('General', 'fan_pin'))
+
 db_host = config.get('General', 'db_host')
 db_port = config.get('General', 'db_port')
 db_name = config.get('General', 'db_name')
+dbClient = None
 
 macs = []
 names = []
 fan_state = 0
-pin = 14
-
-dbClient = InfluxDBClient(host=db_host, port=db_port)
-dbClient.switch_database(db_name)
 
 for tag in config.items('Tags'):
     print(tag)
@@ -40,15 +39,17 @@ print('Listen: ' + str(listen_macs))
 
 # Handle data reception
 def handle_data(found_data):
-    global fan_state
+    global fan_state, dbClient
     found_mac = found_data[0]
     idx = macs.index(found_mac)
     found_name = names[idx]
     temperature = found_data[1]['temperature']
-    print ('mac:' + str(found_data[1]['mac']) +
-        ' batt:' + str(found_data[1]['battery']) +
-        ' temp:' + str(temperature) +
-        ' fan:' + str(fan_state)
+    print ('mac:'+str(found_data[1]['mac']),
+        'batt:'+str(found_data[1]['battery']),
+        'temp:'+str(temperature),
+        'min:'+str(temp_min),
+        'max:'+str(temp_max),
+        'fan:'+str(fan_state)
     )
     if temperature > temp_max and fan_state == 0:
         print('fan started')
@@ -69,17 +70,28 @@ def handle_data(found_data):
             }
         }
     ]
-    dbClient.write_points(json_body)
+    try: 
+        if dbClient:
+            dbClient.write_points(json_body)
+        else:
+            dbClient = InfluxDBClient(host=db_host, port=db_port)
+            dbClient.switch_database(db_name)
+    except (InfluxDBClientError) as err:
+        print ('InfluxDBClientError: ' + err)
+        
+    except (InfluxDBServerError) as err:
+        print ('InfluxDBServerError: ' + err)
+        
 
 try:
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, GPIO.LOW)
     RuuviTagSensor.get_datas(handle_data, listen_macs)
+
 except KeyboardInterrupt:
-    print('Interrupted!')
-except:
-    print('Some error!')
+    print('Exiting!')
+
 finally:
     print('Cleaning up')
     GPIO.cleanup()
